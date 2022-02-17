a9dfed48f6031a0d263ff87d2e7aa7bf4757b219
RoMcGill <ro.mcgill12@gmail.com>
2022-02-17 17:39:32 +0000

    add doc strings

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,13 @@ document.addEventListener("DOMContentLoaded",function(){
        })
     }
 })
-
+/**
+ * the main game loop, called when the script is first loaded 
+ * and after the users answer has been processed 
+ */
 function runGame() {
+    //creates 2 random numbers between 1 and 25
+
     let num1 = math.floor(math.random() * 25) +1;
     let num2 = math.floor(math.random() * 25) +1;
 
