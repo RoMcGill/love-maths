3149e03beea5d4c11cdfb19a50884747ca9f61ea
RoMcGill <ro.mcgill12@gmail.com>
2022-02-17 17:26:54 +0000

    add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,44 @@
-function runGame(){
+// wait for dom to finish loading before running the game 
+//get the button elements and add event listeners
+
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+       button.addEventListener("click",function(){
+           if (this.getAttribute("data-type") ==="submit"){
+               alert("you clicked submit");
+
+           } else {
+               let gameType = this.getAttribute("data-type");
+               alert(`you clicked ${gameType}`);
+           }
+       })
+    }
+})
+
+function runGame() {
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
-function incrementScore(){
+function incrementScore() {
 
 }
-function incrementWrongAnswer(){
+function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion() {
 
 }
-function displaySubtractQuestion(){
+function displaySubtractQuestion() {
 
 }
-function displayMultolyQuestion(){
+function displayMultolyQuestion() {
 
 }
