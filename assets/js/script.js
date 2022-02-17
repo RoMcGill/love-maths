49947640c4a4315045e169f53149789a8574ea79
RoMcGill <ro.mcgill12@gmail.com>
2022-02-17 18:00:03 +0000

    add random numbers and addition questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,30 @@ document.addEventListener("DOMContentLoaded",function(){
 
            } else {
                let gameType = this.getAttribute("data-type");
-               alert(`you clicked ${gameType}`);
+               runGame(gameType);
            }
        })
     }
+
+    runGame("addition");
 })
 /**
  * the main game loop, called when the script is first loaded 
  * and after the users answer has been processed 
  */
-function runGame() {
+function runGame(gameType) {
+   
     //creates 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 
-    let num1 = math.floor(math.random() * 25) +1;
-    let num2 = math.floor(math.random() * 25) +1;
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
 
+    }else {
+        alert(`unknown game type: ${gameType}`);
+        throw`unknown game type: ${gameType}.Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -40,8 +49,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 function displaySubtractQuestion() {
 
