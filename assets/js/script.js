ed5a62198ef61da93a69ff96724a4e51042fe57e
RoMcGill <ro.mcgill12@gmail.com>
2022-02-18 17:46:45 +0000

    add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,8 +41,12 @@ function runGame(gameType) {
 
     }else if (gameType ==="multiply"){
         displayMultiplyQuestion(num1, num2);
+
     }else if (gameType === "subtract"){
         displaySubtractQuestion(num1,num2);
+
+    }else if (gameType === "divide"){
+        displayDivideQuestion(num1, num2);
     }
      else {
         alert(`unknown game type: ${gameType}`);
@@ -82,6 +86,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "divide"];
     }
      else{
         alert(`unimplemented operator and ${operator}`);
@@ -124,4 +130,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 
+}
+function displayDivideQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
