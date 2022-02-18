8e9643f258a01e13e1aee3f5b6a649b8e475e7d1
RoMcGill <ro.mcgill12@gmail.com>
2022-02-18 18:07:18 +0000

    made easy numbers max 12

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,8 +33,8 @@ function runGame(gameType) {
     document.getElementById("answer-box").focus();
 
     //creates 2 random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) +1;
-    let num2 = Math.floor(Math.random() * 25) +1;
+    let num1 = Math.floor(Math.random() * 11) +1;
+    let num2 = Math.floor(Math.random() * 11) +1;
 
     if(gameType === "addition" ){
         displayAdditionQuestion(num1, num2);
@@ -133,7 +133,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 function displayDivideQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1 * operand2 ;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
